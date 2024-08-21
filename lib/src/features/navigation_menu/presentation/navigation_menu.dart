59918adddcdfe530fccbe10926edd41b7009ca4e
metinilgar <metinilgarmutlu@gmail.com>
2024-08-22 00:39:32 +0300

    Add animation to AppBar and NavigationBar with hidable dependency
    - Add hidable dependency to pubspec.yaml
    - Add animation to AppBar and NavigationBar

--- lib/src/features/navigation_menu/presentation/navigation_menu.dart
+++ lib/src/features/navigation_menu/presentation/navigation_menu.dart
@@ -4,65 +4,80 @@ import 'package:x_clone_flutter/src/features/navigation_menu/controller/navigati
 import 'package:x_clone_flutter/src/features/notifications/notifications_screen.dart';
 import 'package:x_clone_flutter/src/features/search/search_screen.dart';
 import 'package:x_clone_flutter/src/features/tweet/presentation/tweet_list_screen.dart';
+import 'package:hidable/hidable.dart';
 
-class NavigationMenu extends ConsumerWidget {
+class NavigationMenu extends ConsumerStatefulWidget {
   const NavigationMenu({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<NavigationMenu> createState() => _NavigationMenuState();
+}
+
+class _NavigationMenuState extends ConsumerState<NavigationMenu> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
     final selectedIndex = ref.watch(navigationControllerProvider);
 
     return Scaffold(
-      appBar: AppBar(
-          surfaceTintColor: Colors.transparent,
-          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-          centerTitle: true,
-          title: const SizedBox(width: 25, height: 25, child: Placeholder())),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          border: Border(
-            top: BorderSide(color: Theme.of(context).dividerTheme.color!),
+      appBar: Hidable(
+        preferredWidgetSize: const Size.fromHeight(80),
+        controller: scrollController,
+        child: AppBar(
+            surfaceTintColor: Colors.transparent,
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            centerTitle: true,
+            title: const SizedBox(width: 25, height: 25, child: Placeholder())),
+      ),
+      bottomNavigationBar: Hidable(
+        controller: scrollController,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Theme.of(context).dividerTheme.color!),
+            ),
           ),
-        ),
-        child: NavigationBar(
-          indicatorColor: Colors.transparent,
-          height: 50,
-          selectedIndex: selectedIndex,
-          labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-          onDestinationSelected: ref
-              .read(navigationControllerProvider.notifier)
-              .onDestinationSelected,
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Icons.home_outlined),
-              selectedIcon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            NavigationDestination(
-              icon: Icon(
-                Icons.search,
+          child: NavigationBar(
+            indicatorColor: Colors.transparent,
+            height: 50,
+            selectedIndex: selectedIndex,
+            labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            onDestinationSelected: ref
+                .read(navigationControllerProvider.notifier)
+                .onDestinationSelected,
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.home_outlined),
+                selectedIcon: Icon(Icons.home),
+                label: 'Home',
               ),
-              selectedIcon: Icon(
-                Icons.search,
-                shadows: <Shadow>[Shadow(blurRadius: 2.0)],
+              NavigationDestination(
+                icon: Icon(
+                  Icons.search,
+                ),
+                selectedIcon: Icon(
+                  Icons.search,
+                  shadows: <Shadow>[Shadow(blurRadius: 2.0)],
+                ),
+                label: 'Search',
               ),
-              label: 'Search',
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.notifications_outlined),
-              selectedIcon: Icon(Icons.notifications),
-              label: 'Notifications',
-            ),
-          ],
+              NavigationDestination(
+                icon: Icon(Icons.notifications_outlined),
+                selectedIcon: Icon(Icons.notifications),
+                label: 'Notifications',
+              ),
+            ],
+          ),
         ),
       ),
       body: IndexedStack(
         index: selectedIndex,
-        children: const [
-          TweetListScreen(),
-          SearchScreen(),
-          NotificationsScreen(),
+        children: [
+          TweetListScreen(scrollController: scrollController),
+          const SearchScreen(),
+          const NotificationsScreen(),
         ],
       ),
     );
--- lib/src/features/tweet/presentation/tweet_list_screen.dart
+++ lib/src/features/tweet/presentation/tweet_list_screen.dart
@@ -3,12 +3,15 @@ import 'package:x_clone_flutter/src/features/tweet/data/dummy_data.dart';
 import 'package:x_clone_flutter/src/features/tweet/presentation/tweet_card.dart';
 
 class TweetListScreen extends StatelessWidget {
-  const TweetListScreen({super.key});
+  const TweetListScreen({super.key, required this.scrollController});
+
+  final ScrollController scrollController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
+          controller: scrollController,
           itemCount: tweets.length,
           itemBuilder: (context, index) => Column(
                 children: [
